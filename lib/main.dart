368d76220f33d3f48f6dde48c3f38200eeca054f
MattiaManneschi <mattia.manneschi@stud.unifi.it>
2024-03-22 16:31:49 +0100

    minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,14 +8,12 @@ var db = FirebaseFirestore.instance.collection("Users");
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid
-      ? await Firebase.initializeApp(
-          options: const FirebaseOptions(
-              apiKey: 'AIzaSyDdfb23B4CSkam7U9DTa36tBiUjI8umdOE',
-              appId: '1:666482531240:android:cc411ad4f2167b6e139b9d',
-              messagingSenderId: '666482531240',
-              projectId: 'composite-helix-414916'))
-      : await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyDdfb23B4CSkam7U9DTa36tBiUjI8umdOE',
+          appId: '1:666482531240:android:cc411ad4f2167b6e139b9d',
+          messagingSenderId: '666482531240',
+          projectId: 'composite-helix-414916'));
   runApp(const MyApp());
 }
 
